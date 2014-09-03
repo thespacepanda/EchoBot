f8e24237fc48747f678888b045563d65fb1c3ef0
Britt Mathis <britt.mathis@gmail.com>
2014-09-03 19:43:33 -0400

    Made EchoBot class more modular - auth and db creation are done outside the class

--- echobot.py
+++ echobot.py
@@ -5,20 +5,16 @@ import tweepy
 import config
 
 class EchoBot(object):
-  def __init__(self):
-    def auth():
-      auth = tweepy.OAuthHandler(config.api_key, config.api_secret)
-      auth.set_access_token(config.token_key, config.token_secret)
-      return tweepy.API(auth)
-    self.tweet_store = anydbm.open("tweet_store", "c")
-    self.twitter = auth()
+  def __init__(self, twitter, tweet_store):
+    self.tweet_store = tweet_store
+    self.twitter = twitter
     self.name = config.BOT
     
   def respond(self):
     for tweet in self.twitter.search(q=self.name):
-      if str(tweet.id) not in self.tweet_store.keys():
+      if str(tweet.id) not in self.tweet_store:
         self.reply(tweet)
-    self.save_state()
+    self.close_db()
 
   def reply(self, tweet):
     def clean_author(tweet):
@@ -32,12 +28,25 @@ class EchoBot(object):
         return text
     self.twitter.update_status(status="%s %s" % (clean_author(tweet), remove_echo(tweet.text)))
     self.tweet_store[str(tweet.id)] = ""
+    self.save_state()
         
   def save_state(self):
+    self.tweet_store.sync()
+
+  def close_db(self):
     self.tweet_store.close()
 
+def authenticate_twitter(config):
+  auth = tweepy.OAuthHandler(config.api_key, config.api_secret)
+  auth.set_access_token(config.token_key, config.token_secret)
+  return tweepy.API(auth)
+
+def tweet_store():
+  return anydbm.open("tweet_store", "c")
+    
 def main():
-  echobot = EchoBot()
+  twitter = authenticate_twitter(config)
+  echobot = EchoBot(twitter, tweet_store())
   echobot.respond()
 
 if __name__ == "__main__":
